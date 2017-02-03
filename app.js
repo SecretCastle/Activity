8e8d66960a2b151d866147b26cd4342ed6a6d55f
SecretCastle <chenfangzhong@hotmail.com>
2017-02-03 09:44:49 +0800

    new

---
+++ app.js
@@ -0,0 +1,14 @@
+App({
+    onLaunch: function () {
+        //String1
+    },
+    onShow: function () {
+        //String2
+    },
+    onHide: function () {
+        //String3
+    },
+    onError: function (msg) {
+        //String4
+    }
+});
