8adb10f5bbaac8f9b050e7ba566a52e20a2fd8c3
SecretCastle <chenfangzhong@hotmail.com>
2017-02-19 21:04:36 +0800

    update

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 App({
     onLaunch: function () {
-        //String1
+        // wx.connectSocket({
+        //     url: 'ws://localhost:8001/'
+        // })
+        // wx.onSocketOpen(function(res){
+        //     console.log('WebSocket连接已打开！')
+        // })
+        // wx.onSocketError(function(res){
+        //     console.log('WebSocket连接打开失败，请检查！')
+        // })
+        // wx.onSocketMessage(function(res){
+        //     console.log(res);
+        // })
     },
     onShow: function () {
         //String2
--- pages/index/index.js
+++ pages/index/index.js
@@ -45,7 +45,7 @@ Page({
     ]
   },
   onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+    
   },
   onReady:function(){
     // 页面渲染完成
@@ -58,5 +58,10 @@ Page({
   },
   onUnload:function(){
     // 页面关闭
+  },
+  sendMSG:function(){
+    // wx.sendSocketMessage({
+    //   data:"hello world"
+    // })
   }
 })
